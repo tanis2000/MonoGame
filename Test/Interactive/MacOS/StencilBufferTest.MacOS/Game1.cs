94c5a2df3d83f1f9fe85d4b868b7d9a5c28e974c
Valerio Santinelli <santinelli@altralogica.it>
2016-01-28 21:33:15 +0100

    Stencil Buffer test for MacOS

---
+++ Test/Interactive/MacOS/StencilBufferTest.MacOS/Game1.cs
@@ -0,0 +1,96 @@
+﻿#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace StencilBufferTest.MacOS {
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game {
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D _maskTex;
+
+		// only write when the stencil buffer has 1
+		public static DepthStencilState EqualStencilState = new DepthStencilState() {
+			StencilEnable = true,
+			StencilFunction = CompareFunction.Equal,
+			StencilPass = StencilOperation.Keep,
+			ReferenceStencil = 1,
+			DepthBufferEnable = false,
+		};
+
+		public Game1 ()
+		{
+			graphics = new GraphicsDeviceManager (this);
+			Content.RootDirectory = "Content";	            
+			graphics.IsFullScreen = true;
+			graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			// TODO: Add your initialization logic here
+			base.Initialize ();
+				
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch (GraphicsDevice);
+			_maskTex = Content.Load<Texture2D>( "mask" );		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Exit() is obsolete on iOS
+			#if !__IOS__ &&  !__TVOS__
+			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+			    Keyboard.GetState ().IsKeyDown (Keys.Escape)) {
+				Exit ();
+			}
+			#endif
+			// TODO: Add your update logic here			
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+		
+			spriteBatch.Begin( SpriteSortMode.Immediate, null, SamplerState.PointClamp, EqualStencilState, null, null );
+			//spriteBatch.Begin();
+			spriteBatch.Draw( _maskTex, new Vector2( 200, 200 ), Color.White );
+			spriteBatch.End();
+
+			base.Draw (gameTime);
+		}
+	}
+}
+
